b8645e98f280eb7489497c4effe2c5af78bebbf0
vihkovihko15 <98320216+vihkovihko15@users.noreply.github.com>
2025-04-15 18:07:07 +0300

    Add files via upload

---
+++ mc-start.sh
@@ -0,0 +1,6 @@
+export DISPLAY=:0
+pkill -f java
+prismlauncher -l 1.21.51 -a sellatar -s celestial.minehut.gg && prismlauncher -l 1.21.52 -a TheStay -s celestial.minehut.gg && prismlauncher -l 1.21.53 -a HydrogenWarlock -s celestial.minehut.gg && prismlauncher -l 1.21.54 -a InclinedWanderer -s celestial.minehut.gg
+
+
+
